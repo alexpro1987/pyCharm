41bff52a43342dfbd289cadce17b235df172d3c9
Alexandr Nosenko <51228215+alexpro1987@users.noreply.github.com>
2020-01-07 22:34:09 -0800

    Added a web crawler by Bucky Roberts thenewboston

---
+++ webcrawler.py
@@ -0,0 +1,45 @@
+# Thanks Bucky Roberts thenewboston for providing Tutorial material of how
+# to build a webcrawler
+# And create a dictionary of the words acquired.
+# Minor changes have been made to the original code
+import requests
+from bs4 import BeautifulSoup
+import operator
+
+
+def start(url):
+    word_list = []
+    source_code = requests.get(url).text
+    soup = BeautifulSoup(source_code, "html.parser")
+    for post_text in soup.findAll('a', {'class': 'dc by kk kl km kn'}):
+        content = post_text.string
+        words = content.lower().split()
+        for each_word in words:
+            word_list.append(each_word)
+    clean_up_list(word_list)
+
+
+def clean_up_list(word_list):
+    clean_word_list = []
+    for word in word_list:
+        symbols = "!@#$%^&*()_+<>?/.:;`~,\'\"[]1234567890"
+        for i in range(0, len(symbols)):
+            word = word.replace(symbols[i], "")
+        if len(word) > 0:
+            clean_word_list.append(word)
+    create_dictionary(clean_word_list)
+
+
+def create_dictionary(clean_word_list):
+    word_count = {}
+    for word in clean_word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    for key, value in sorted(word_count.items(), key=operator.itemgetter(1)):
+        print(key, value)
+
+
+start('https://medium.com/datadriveninvestor/top-15-ai-and-machine-learning-articles-of-2019-1202b5fc7707')
+
